dbe9d8ce4e9c0e904963a8092d32d5bedb7c4a52
Segah Meer <segahm@gmail.com>
2014-05-18 21:35:12 -0700

    changes before sending a letter

--- templates.js
+++ templates.js
@@ -7,7 +7,7 @@ var config = {
 		restaurants: 'Restaurants',
 		counties_home: 'Restaurants, Counties',
 		shortDescription: function(title,valuation,county){
-			return 'How much exactly is '+title+' restaurant worth? We estimate it at $'+valuation+'. Starting with market analysis of '+county+' county we use company-specific data to narrow down the competitive advantages of every restaurant in America.';
+			return 'How much exactly is '+(title.match(/Restaurant/i)?title:title+' restaurant')+' worth? We estimate it at $'+valuation+'. Starting with market analysis of '+county+' county we use company-specific data to narrow down the competitive advantages of every restaurant in America.';
 		},
 		page: {
 			company: {page_title: 'How much is my business worth?'},
@@ -21,23 +21,21 @@ var config = {
 		},
 	},
 	spanish: {
-		cities_home: 'Los restaurantes, Ciudades',
-		restaurants: 'Los restaurantes',
-		counties_home: 'Los restaurantes, Condados',
-		//!!!!!!!!! TO BE CORRECTED
+		cities_home: 'Restaurantes, Ciudades',
+		restaurants: 'Restaurantes',
+		counties_home: 'Restaurantes, Condados',
 		shortDescription: function(title,valuation,county){
-			return 'How much exactly is '+title+' restaurant worth? We estimate it at $'+valuation+'. Starting with market analysis of '+county+' county we use company-specific data to narrow down the competitive advantages of every restaurant in America.';
+			return '¿Cuánto exactamente '+(title.match(/Restaurant|restaurante/i)?title:title+' restaurante')+'? Lo estimamos a $'+valuation+'. A partir de analisis del mercado '+county+'  condado utilizamos datos especificos de las empresas para restringir las ventajas competitivas de cada restaurante en los Estados Unidos.';
 		},
-		//!!!!!!! TO BE CORRECTED
 		page: {
-			company: {page_title: '¿Cuál es el valor de mi empresa?'},
-			directory: {page_title: 'Finanzas para pequeñas empresas y de valoración en'},
-			index: {page_title: 'Borrow Goldfish: las finanzas de pequeñas empresas y la valoración'}
+			company: {page_title: '¿Cuánto exactamente vale mi empresa?'},
+			directory: {page_title: 'Finanzas de las pequeñas empresas y la valoración en'},
+			index: {page_title: 'Borrow Goldfish: Finanzas de las pequeñas empresas y la valoración'}
 		},
 		all_pages: {
-			twitter_share_text: encodeURIComponent('I might be running a #smallbiz, but my numbers speak for themselves'),
-			encoded_meta_description: encodeURIComponent('We perform business valuation analysis on thousands of small businesses. Learn how your company is valued. Learn about restaurant business valuation.'),
-			meta_description: 'We perform business valuation analysis on thousands of small businesses. Learn how your company is valued. Learn about restaurant business valuation.'
+			twitter_share_text: encodeURIComponent('Yo podría ejecutarse una #PYMES, pero mis números hablan por sí mismos'),
+			encoded_meta_description: encodeURIComponent('Realizamos análisis de valuación de negocios en miles de pequeñas empresas. Aprenda cómo su empresa se valora. Aprender sobre valoración de empresas de restaurante.'),
+			meta_description: 'Realizamos análisis de valuación de negocios en miles de pequeñas empresas. Aprenda cómo su empresa se valora. Aprender sobre la valoración de empresas en la restauración.'
 		},
 	}
 };
