52cb35e7f9b0f7871cbc872de601aadb400e4ffd
Segah Meer <segahm@gmail.com>
2014-06-14 15:12:59 -0700

    testing git push web

--- test.js
+++
@@ -1,7 +0,0 @@
-var Crawler = require('./crawler-app');
-var fs = require('fs');
-
-fs.readFile('./Crawler/site.html', function (err, html) {
-  if (err) throw err;
-  console.log(Crawler.prototype.processDocument(html));
-});
