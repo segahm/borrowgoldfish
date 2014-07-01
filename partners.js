725a89157c95664fcdb96b9cc395cd1b407cb93a
Segah Meer <segahm@gmail.com>
2014-07-01 08:48:26 -0700

    removed onDeck temporarily

--- partners.js
+++ partners.js
@@ -1,12 +1,32 @@
 var PARTNERS = {
-    nonlenders: [{name: 'Credit Analytics',value: '$5 at ...', link: 'http://www.google.com'}],
-    lenders: [
-        {name: 'swiftcapital.com',value: 'Smallest sized loans (15k-50k) at relatively low rates (10.5% avg.) with fastest turnaround time during approval with O.K. duration (1 year max)', link: 'https://www.swiftcapital.com/fastfunding/google/Home.html?ref=caura'},
-        {name: 'Kabbage',value: 'If you are desperate... Expensive (avg. of 55% with cases of 125%) for typically no more than $10k and for no longer than 6 months', link: 'https://www.kabbage.com/splp/kabbagesimpleppalsem'},
-        {name: 'LendingClub',value: 'Lowest interest rates (5.9% with avg of 12.5%) for medium-sized projects (avg. of $57,000) and for longest duration (up to 5 years)', link: 'https://www.lendingclub.com/business/#!/landing/a2'},
-        {name: 'onDeck.com',value: 'Very expensive (18%-50%) capital for long duration (3 months to 2.5 years) and for projects of all sizes ($5,000-$250,000)', link: 'https://www.ondeck.com/'},
-        {name: 'Capital One Small Business',value: 'Credit line only products ranging from 9% to 25% (at 15% avg.) for 10k-250k (slowest time to funding, 10-11 days on avg.)', link: 'http://www.capitalone.com/business-credit-cards/'},
-        {name: 'cancapital',value: 'Big loans (up to $250,000) but realy short-term (7-30 days) and that are relatively expensive (30%)', link: 'http://www.cancapital.com/resources/apply/business-loans'},
+    nonlenders: [{
+        name: 'Credit Analytics',
+        value: '$5 at ...',
+        link: 'http://www.google.com'
+    }],
+    lenders: [{
+            name: 'swiftcapital.com',
+            value: 'Smallest sized loans (15k-50k) at relatively low rates (10.5% avg.) with fastest turnaround time during approval with O.K. duration (1 year max)',
+            link: 'https://www.swiftcapital.com/fastfunding/google/Home.html?ref=caura'
+        }, {
+            name: 'Kabbage',
+            value: 'If you are desperate... Expensive (avg. of 55% with cases of 125%) for typically no more than $10k and for no longer than 6 months',
+            link: 'https://www.kabbage.com/splp/kabbagesimpleppalsem'
+        }, {
+            name: 'LendingClub',
+            value: 'Lowest interest rates (5.9% with avg of 12.5%) for medium-sized projects (avg. of $57,000) and for longest duration (up to 5 years)',
+            link: 'https://www.lendingclub.com/business/#!/landing/a2'
+        },
+        /*{name: 'onDeck.com',value: 'Very expensive (18%-50%) capital for long duration (3 months to 2.5 years) and for projects of all sizes ($5,000-$250,000)', link: 'https://www.ondeck.com/'},*/
+        {
+            name: 'Capital One Small Business',
+            value: 'Credit line only products ranging from 9% to 25% (at 15% avg.) for 10k-250k (slowest time to funding, 10-11 days on avg.)',
+            link: 'http://www.capitalone.com/business-credit-cards/'
+        }, {
+            name: 'cancapital',
+            value: 'Big loans (up to $250,000) but realy short-term (7-30 days) and that are relatively expensive (30%)',
+            link: 'http://www.cancapital.com/resources/apply/business-loans'
+        },
     ]
 };
 
